4a22ae0994dd973dafa49c051ecd07ebb4b5b651
Bruno Peixoto <brunolnetto@gmail.com>
2022-01-08 19:34:11 -0300

    server modified

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 // [START app]
 const express = require('express');
-const { Graph, addEdge, allPaths, allVicinity, looseNodes } = require('./utils/directed_graph.js');
+const { Graph } = require('./utils/directed_graph.js');
 
 const app = express();
 
@@ -23,25 +23,25 @@ app.get('/', (req, res) => {
   // Create a sample graph
   // const graph = req.blueprint.nodes;
   
-  Graph(4);
-  addEdge(0, 1);
-  addEdge(0, 2);
-  addEdge(0, 3);
-  addEdge(2, 0);
-  addEdge(2, 1);
-  addEdge(1, 3);
+  let s = Graph(4);
+  s.addEdge(0, 1);
+  s.addEdge(0, 2);
+  s.addEdge(0, 3);
+  s.addEdge(2, 0);
+  s.addEdge(2, 1);
+  s.addEdge(1, 3);
 
   // arbitrary source
-  let s = 3;
+  let from_ = 1;
 
   // arbitrary destination
-  let d = 1;
+  let to = 3;
 
-  let all_paths = allPaths(s, d);
-  let all_vicinity = allVicinity();
-  let loose_nodes = looseNodes();
+  let all_paths = s.allPaths(from_, to);
+  let all_vicinity = s.allVicinity();
+  let loose_nodes = s.looseNodes();
 
-  console.log('Paths from '+s+' to '+d+' :');
+  console.log('Paths from '+from_+' to '+to+' :');
   console.log(all_paths);
   
   console.log('Reachibility: ');
@@ -50,12 +50,12 @@ app.get('/', (req, res) => {
   console.log('Loose nodes: ');
   console.log(loose_nodes);
 
-  let text_ = "Original code from the URL:  <a href=https://www.geeksforgeeks.org/find-paths-given-source-destination//> Graph paths original post </a> " + "<br>";
+  let text_ = "Original code from the URL :  <a href=https://www.geeksforgeeks.org/find-paths-given-source-destination//> Graph paths original post </a> " + "<br>";
   
   if(all_paths.length==0){
-    text_ = text_ + 'There is no path available from '+s+' to '+d+'!';
+    text_ = text_ + 'There is no path available from ' + from_ + ' to ' + to + '!';
   } else {
-    text_ = text_ + "Following are all different paths from " + s + " to " + d + "<br>";
+    text_ = text_ + "Following are all different paths from " + from_ + " to " + to + "<br>";
     
     for (let i=0; i<all_paths.length; i++) {
       text_ = text_ + all_paths[i] + "<br>";
