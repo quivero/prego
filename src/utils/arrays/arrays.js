6cf8f17a8d18af78844c966b4d280ee731d14846
Bruno Peixoto <brunolnetto@gmail.com>
2022-01-14 17:01:13 -0300

    unique elements

--- src/utils/arrays/arrays.js
+++ src/utils/arrays/arrays.js
@@ -1,6 +1,7 @@
 
 export const getAllIndexes = (arr, val) => {
     var indexes = [], i;
+    
     for(i = 0; i < arr.length; i++)
         if (arr[i] === val)
             indexes.push(i);
@@ -17,4 +18,25 @@ export const cyclicSort = (array, index) => {
     let tail = array.slice(0, index);
 
     return head.concat(tail);
+}
+
+export const isCyclicEqual = (control_, treatment_) => {
+    let control_len = control_.length;
+    let treatment_len = treatment_.length;
+
+    if(control_len != treatment_len){
+        return false;
+    }
+
+    for(let i=0; i<treatment_len; i++){
+        if(cyclicSort(treatment_, i) == control_){
+            return true
+        }
+    }
+
+    return false;
+}
+
+export const getUniques = (vec) => {
+    return Array.from(new Set(vec));
 }
