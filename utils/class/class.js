57200b67dc1f6794be838b11365edd1edbc3333f
Bruno Peixoto <brunolnetto@gmail.com>
2022-01-24 18:51:42 -0300

    fix/ eslint fixes

--- utils/class/class.js
+++
@@ -1,6 +0,0 @@
-export const clone = (reference) => {
-    let proto = Object.getPrototypeOf(reference);
-    let model = Object.create(proto);
-    
-    return Object.assign(model, reference)
-}
--- utils/iter/iter.js
+++
@@ -1,4 +0,0 @@
-export const iterToArray = (iter) => {
-    let iter_arr = new Array([...iter])
-    return iter_arr[0]
-}
