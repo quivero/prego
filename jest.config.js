b881f22aba9537b3eba8f2ba6e61064bd30abfbe
Bruno Peixoto <brunolnetto@gmail.com>
2022-01-26 22:00:10 -0300

    config/ jest config file

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,7 @@ export default {
   verbose: false,
 
   // Indicates whether the coverage information should be collected while executing the test
-  collectCoverage: false,
+  collectCoverage: true,
 
   // The directory where Jest should output its coverage files.
   coverageDirectory: './coverage/',
