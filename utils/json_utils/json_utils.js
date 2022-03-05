954ba46f27e510514ac310e89fca1d1fd4abacc8
Astolfo <brunolnetto@gmail.com>
2022-03-05 19:44:09 -0300

    feat/ save object to file on path

--- utils/json_utils/json_utils.js
+++ utils/json_utils/json_utils.js
@@ -1,6 +1,8 @@
+import fs from 'fs';
+
 export const saveJSONtoFile = (path, json_object, name) => {
   // convert JSON object to string
-  const json_str = JSON.stringify(json_object);
+  const json_str = JSON.stringify(json_object, null, 2);
 
   // write JSON string to a file
   fs.writeFile(name, json_str, (err) => {
