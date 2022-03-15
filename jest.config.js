daa1bde11301c626248590ef35d35e3a51d87fac
Astolfo <brunolnetto@gmail.com>
2022-03-14 22:44:52 -0300

    config/ jest config file

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,12 @@
-export default {
+// Sync object
+/** @type {import('@jest/types').Config.InitialOptions} */
+const config = {
+  verbose: true,
+
   // The bail config option can be used here to have Jest stop running tests after
   // the first failure.
   bail: false,
-
-  // Indicates whether each individual test should be reported during the run.
-  verbose: false,
-
+  
   // Indicates whether the coverage information should be collected while executing the test
   collectCoverage: true,
 
@@ -36,3 +37,6 @@ export default {
     },
   },
 };
+
+module.exports = config;
+
