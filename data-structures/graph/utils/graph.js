00606b6514268ae175e1c97bf8cbd2d8a7857873
Astolfo <brunolnetto@gmail.com>
2022-03-15 20:02:19 -0300

    refac/feat/ create{vertices, edges} moved to graph utils

---
+++ data-structures/graph/utils/graph.js
@@ -0,0 +1,53 @@
+import GraphVertex from '../GraphVertex.js'
+import GraphEdge from '../GraphEdge.js'
+
+import { 
+  objectInit,
+  objectMap,
+ } from '../../../utils/objects/objects.js';
+
+import _ from 'lodash';
+
+export const createVertices = (labels) => {
+    const vertices = [];
+  
+    labels.forEach((label) => {
+      const vertex = new GraphVertex(label);
+      vertices.push(vertex);
+    });
+  
+    return vertices;
+  };
+
+export const createEdges = (vertices_tuples) => {
+    const edges = [];
+  
+    vertices_tuples.forEach((vertices_tuple) => {
+      const edge = new GraphEdge(vertices_tuple[0], vertices_tuple[1]);
+      edges.push(edge);
+    });
+  
+    return edges;
+  };
+
+  export const createEdgesFromVerticesValues = (vertices_values_tuples) => {
+    const vertex_id_to_obj = objectMap(
+      objectInit(
+        _.uniq(_.flatten(vertices_values_tuples)),
+        {}
+      ),
+      (vertex_key, obj) => {
+        return new GraphVertex(vertex_key)
+      }
+    )
+    
+    return createEdges(
+      vertices_values_tuples.map(
+        (vertices_values_tuple) => {
+          return [
+            vertex_id_to_obj[vertices_values_tuple[0]],
+            vertex_id_to_obj[vertices_values_tuple[1]]
+          ]
+        })
+    )
+  };
