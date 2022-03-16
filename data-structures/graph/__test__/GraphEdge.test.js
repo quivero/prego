efe5886cb1c506c9107be578dbc726a1f40b381a
Astolfo <brunolnetto@gmail.com>
2022-03-16 17:32:48 -0300

    test/ complete graph tests

--- data-structures/graph/__test__/GraphEdge.test.js
+++ data-structures/graph/__test__/GraphEdge.test.js
@@ -2,6 +2,8 @@ import GraphEdge from '../GraphEdge';
 
 import {
   createVertices,
+  createEdgesFromVerticesValues,
+  createCompleteUndirectedGraph
 } from '../utils/graph.js';
 
 describe('GraphEdge', () => {
@@ -38,5 +40,19 @@ describe('GraphEdge', () => {
     expect(edge.startVertex).toEqual(vertexB);
     expect(edge.endVertex).toEqual(vertexA);
     expect(edge.weight).toEqual(10);
+  });
+
+  it('should be possible to do edge reverse', () => {
+    const [edgeAB] = createEdgesFromVerticesValues([['A', 'B']]);
+    
+    expect(edgeAB.startVertex.getKey()).toEqual('A');
+    expect(edgeAB.endVertex.getKey()).toEqual('B');
+    expect(edgeAB.weight).toEqual(0);
+  });
+
+  it('should be possible to do edge reverse', () => {
+    const graph = createCompleteUndirectedGraph(['A', 'B', 'C']);
+    
+    expect(Object.keys(graph.edges).length).toEqual(3);
   });
 });
