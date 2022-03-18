4d7c895f574c3ecbbdf88316494429125a68a946
Astolfo <brunolnetto@gmail.com>
2022-03-18 18:55:45 -0300

    docs/ documentation

--- utils/json_utils/json_utils.js
+++ utils/json_utils/json_utils.js
@@ -1,5 +1,12 @@
 import fs from 'fs';
 
+/**
+ * @abstract saves json to file
+ * 
+ * @param {Array} path
+ * @param {Object} json_object
+ * @param {String} name
+ */
 export const saveJSONtoFile = (path, json_object, name) => {
   // convert JSON object to string
   const json_str = JSON.stringify(json_object, null, 2);
@@ -14,6 +21,12 @@ export const saveJSONtoFile = (path, json_object, name) => {
   });
 };
 
+/**
+ * @abstract loads json to file
+ * 
+ * @param {Array} path
+ * @param {Object} name
+ */
 export const loadJSONfromFile = (path, name) => {
   const filedata = fs.readFileSync(`${path + name}.json`, 'utf8', (error, data) => {
     if (error) {
