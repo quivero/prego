154215e3b801962c70e7455f2d85691240ed4d20
Astolfo <brunolnetto@gmail.com>
2022-03-22 16:34:31 -0300

    feat/test/ Mesh grid from graph

--- data-structures/mesh/Mesh.js
+++ data-structures/mesh/Mesh.js
@@ -1,4 +1,5 @@
 import Graph from '../graph/Graph';
+import MeshVertex from '../mesh/MeshVertex';
 
 export default class Mesh extends Graph {
   #metric;
@@ -6,30 +7,35 @@ export default class Mesh extends Graph {
   /**
    * @param {boolean} isDirected
    */
-  constructor(isDirected = false, metric_function, is_relaxed) {
+  constructor(metric_function, isDirected = false, is_relaxed = false) {
     super(isDirected);
     this.coordinates = {};
 
     function metricIsValid(metric_function) {
-      // TAKE NOTE: This validation is weak. A random generator may suit better than it
-      const P0 = new CoordinatePoint('P0', [0, 0]);
-      const P1 = new CoordinatePoint('P1', [0, 1]);
-      const P2 = new CoordinatePoint('P2', [1, 0]);
-
-      return metric_function(P0, P0) === 0
-             && metric_function(P0, P1) === metric_function(P1, P0)
-             && metric_function(P0, P2) <= metric_function(P0, P1) + metric_function(P1, P2);
+      // TAKE NOTE: This validation is weak. Either random generator or a user-provided validator function
+      // may suit better than it
+      const P0 = new MeshVertex('P0', [0, 0]);
+      const P1 = new MeshVertex('P1', [0, 1]);
+      const P2 = new MeshVertex('P2', [1, 0]);
+      
+      const is_positive = metric_function(P1, P1) === 0;
+      const is_symmetric = metric_function(P0, P1) === metric_function(P1, P0);
+      const is_triangular =  metric_function(P0, P2) <= metric_function(P0, P1) + metric_function(P1, P2);
+            
+      return is_positive && is_symmetric && is_triangular;
     }
 
-    if (!is_relaxed && metricIsValid(metric_function)) {
-      this.metric_function = metric_function;
-    } else {
-      const statement = 'Metric function is not valid. It must obey :';
-      const condition_1 = '1. Value 0 between identical vertices;';
-      const condition_2 = '2. Symmetrical;';
-      const condition_3 = '3. Triangular inequality';
+    if (!is_relaxed) {
+      if(metricIsValid(metric_function)) {
+        this.metric_function = metric_function;
+      } else {
+        const statement = 'Metric function is not valid. It must obey :';
+        const condition_1 = '1. Value 0 between identical vertices;';
+        const condition_2 = '2. Symmetrical;';
+        const condition_3 = '3. Triangular inequality';
 
-      throw Error(`${statement}\n${condition_1}\n${condition_2}\n${condition_3}`);
+        throw Error(`${statement}\n${condition_1}\n${condition_2}\n${condition_3}`);
+      }
     }
   }
 
--- data-structures/mesh/__test__/Mesh.test.js
+++ data-structures/mesh/__test__/Mesh.test.js
@@ -2,6 +2,19 @@ import Mesh from '../Mesh';
 
 describe('Mesh', () => {
   it('should contain mesh tests', () => {
-    expect(true).toBe(true);
+    let metric_fun = (coord_1, coord_2) => {
+      return Math.sqrt(
+        Math.pow(
+          coord_2.coordinates[1]-coord_1.coordinates[1], 2) + 
+        Math.pow(
+          coord_2.coordinates[0]-coord_1.coordinates[0], 2
+        )
+        )
+    }
+
+    let mesh = new Mesh(metric_fun)
+    
+    expect(mesh).toBeDefined();
+
   });
 });
