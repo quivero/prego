9220f1023d40c3794ebf2260c3542d15784bba2f
Bruno Peixoto <brunolnetto@gmail.com>
2022-04-14 14:46:32 -0300

    misc/ src file with activated all_path

--- src/index.js
+++ src/index.js
@@ -9,6 +9,11 @@ import date from 'date-and-time';
 
 import {
   parseBlueprintToGraph,
+  startAndFinishNodes,
+  getBlueprintFromToEdgeTuples,
+  getBlueprintNextNodes,
+
+  fromStartToFinishCombsAllPaths,
 } from '../utils/workflow/parsers.js';
 
 import {
@@ -18,10 +23,6 @@ import {
 import {
   partitions,
 } from '../utils/combinatorics/partition.js';
-
-import {
-  fromStartToFinishCombsAllPaths,
-} from '../utils/workflow/parsers.js';
 
 const require = createRequire(import.meta.url);
 const app = express();
@@ -45,20 +46,22 @@ app.get('/', (req, res) => {
   const blueprints_fnames = fs.readdirSync(bps_root);
 
   const READ_ALL_BPS = true;
+  let sf_nodes = {};
+  let loose_nodes = [];
+  let orphan_nodes = [];
 
   if (READ_ALL_BPS) {
     let paths = {};
     let total_paths_len = 0;
 
     for (let i = 0; i < blueprints_fnames.length; i += 1) {
-      const blueprint_i_name = blueprints_fnames[i];
-      const fname = bps_root + blueprint_i_name;
-      const tokens = fname.split('.');
+      let blueprint_i_name = blueprints_fnames[i];
+      let fname = bps_root + blueprint_i_name;
+      let tokens = fname.split('.');
 
       if (tokens[tokens.length - 1] === 'json') {
-        const blueprint_i = require(fname);
-
-        const graph = parseBlueprintToGraph(blueprint_i);
+        let blueprint_i = require(fname);
+        let graph = parseBlueprintToGraph(blueprint_i);
 
         paths[blueprint_i_name] = fromStartToFinishCombsAllPaths(blueprint_i);
         total_paths_len += paths[blueprints_fnames[i]].length;
@@ -72,26 +75,12 @@ app.get('/', (req, res) => {
       },
     );
   } else {
-    const blueprint_fname = 'propertiesCRUD.json';
+    const blueprint_fname = 'botMessage.json';
 
-    const fname = bps_root + blueprint_fname;
-    const blueprint = require(fname);
-    const graph = parseBlueprintToGraph(blueprint);
+    let fname = bps_root + blueprint_fname;
+    let blueprint = require(fname);
 
-    const a = [1, 2, 3, 4, 5];
-    const b = [1, 3, 5, 7, 9];
-    const c = [2, 4, 6, 8, 10];
-    const d = [1, 2, 4, 5, 7, 8];
-    const e = [1, 2, 5, 6, 9, 10];
-    const f = [8, 9, 11, 12, 14, 15];
-    const g = [9, 10, 12, 13, 15, 16];
-    const h = [8, 11, 12, 15, 18, 20];
-    const i = [7, 10, 13, 14, 15, 17];
-    const j = [11, 12, 13, 14, 15, 16];
-    const k = [10, 11, 14, 15, 18, 19];
-    const l = [1, 5, 10, 15, 17, 20];
-
-    res.send(spreadEuler([a, b, c, d, e, f, g, h, i, j, k, l]));
+    res.send(fromStartToFinishCombsAllPaths(blueprint));
   }
 });
 // [END app]
