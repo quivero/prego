e5bda61a620c26fe89f0a4e37443c5b7d0f34835
Bruno Peixoto <brunolnetto@gmail.com>
2022-04-17 18:44:18 -0300

    feat/ factor number in prime numbers

--- utils/numbers/numbers.js
+++ utils/numbers/numbers.js
@@ -1,3 +1,19 @@
-export const bigIntGenerator = () => Array(16).fill()
-  .map(() => Math.round(Math.random() * 0xF).toString(16))
-  .join('');
+import {
+  countDict,
+} from '../arrays/arrays.js'
+
+export const primeFactors = (n) => {
+  const factors = [];
+  let divisor = 2;
+
+  while (n >= 2) {
+    if (n % divisor == 0) {
+      factors.push(divisor);
+      n = n / divisor;
+    } else {
+      divisor++;
+    }
+  }
+  
+  return countDict(factors);
+}
