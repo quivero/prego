51f000925ef54e83da06fdd7c653049f02d257e2
Bruno Peixoto <brunolnetto@gmail.com>
2022-04-19 18:15:30 -0300

    refac/ add exception for primefactor of negative numbers and with decimal part unequal zero

--- utils/numbers/numbers.js
+++ utils/numbers/numbers.js
@@ -2,7 +2,15 @@ import {
   countDict,
 } from '../arrays/arrays.js'
 
+import {
+  decimalPart,
+} from '../math/math.js'
+
 export const primeFactors = (n) => {
+  if(decimalPart(n) !== 0 || n < 0) {
+    throw Error('The provided number must not be zero and must be natural.');
+  }
+  
   const factors = [];
   let divisor = 2;
 
