fb7446a077b79a25613c77c39023069d6c959a76
Bruno Peixoto <brunolnetto@gmail.com>
2022-05-03 14:52:18 -0300

    feat/ logging file

---
+++ utils/logging/logger.js
@@ -0,0 +1,25 @@
+import { 
+    createLogger, 
+    format, 
+    transports, 
+    config 
+} from 'winston'
+
+export const logger = createLogger({
+  level: "debug",
+  levels: config.npm.levels,
+  format: format.combine(
+    format.padLevels(),
+    format.timestamp({ format: "DD/MM/YYYY HH:mm:ss" }),
+    format.colorize(),
+    format.printf((info) => `[${info.timestamp}] ${info.level}: ${info.message}`)
+  ),
+  transports: [new transports.Console()],
+  exceptionHandlers: [
+    new transports.Console({
+      format: format.errors(),
+    }),
+  ],
+  rejectionHandlers: [new transports.Console()],
+});
+
