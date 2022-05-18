db21e85f47abf78522e5e29acf4c372e1b7b25f5
Bruno Peixoto <brunolnetto@gmail.com>
2022-05-18 17:56:23 -0300

    feat/ create directory, save content from object as name-content tuple, load json file and verify existence of extension on filename

--- utils/file/file.js
+++ utils/file/file.js
@@ -1,23 +1,32 @@
 import fs from 'fs';
 
-export const createDirectory = (path, name) => {
-  const folder_path = `${path}/${name}`
+import {
+  objectForEach,
+} from '../objects/objects.js';
+
+export const createDirectory = (root_path, name) => {
+  const folder_path = `${root_path}/${name}`
   
   if (fs.existsSync(folder_path)) {
-    console.log('Directory already exists!');
+    console.log(`Directory ${root_path}/${name} already exists!`);
   } else {
-    fs.mkdir(
+    fs.mkdirSync(
       folder_path, 
       (err) => {
         if (err) {
             return console.error(err);
         }
-        console.log('Directory created successfully!');
+
+        console.log(`Directory ${name} created successfully at path ${root_path}!`);
       }
     );
   }
-  
-  
+}
+
+export const saveFilenameContentObject = (obj, root_path) => {
+  objectForEach(
+    obj, (filename, content) => saveStringtoFile(`${root_path}`, `${filename}`, content)
+  )
 }
 
 /**
@@ -34,7 +43,7 @@ export const saveStringtoFile = (path, name, string) => {
       throw err;
     }
     
-    console.log('String is saved.');
+    console.log(`String saved at file ${path}/${name}.txt`);
   });
 };
 
@@ -45,17 +54,17 @@ export const saveStringtoFile = (path, name, string) => {
  * @param {Object} json_object
  * @param {String} name
  */
-export const saveJSONtoFile = (path, json_object, name) => {
+export const saveJSONtoFile = (root_path, json_object, name) => {
   // convert JSON object to string
   const json_str = JSON.stringify(json_object, null, 2);
 
   // write JSON string to a file
-  fs.writeFile(`${path}/${name}.json`, json_str, (err) => {
+  fs.writeFile(`${root_path}/${name}.json`, json_str, (err) => {
     if (err) {
       throw err;
     }
 
-    console.log('JSON data is saved.');
+    console.log(`JSON file saved at file ${root_path}/${name}.json`);
   });
 };
 
@@ -65,8 +74,8 @@ export const saveJSONtoFile = (path, json_object, name) => {
  * @param {Array} path
  * @param {Object} name
  */
-export const loadJSONfromFile = (path, name) => {
-  const filedata = fs.readFileSync(`${path + name}.json`, 'utf8', (error, data) => {
+export const loadJSONfromFile = (root_path, name) => {
+  const filedata = fs.readFileSync(`${root_path + name}.json`, 'utf8', (error, data) => {
     if (error) {
       console.log(error);
     }
@@ -74,3 +83,8 @@ export const loadJSONfromFile = (path, name) => {
 
   return JSON.parse(filedata);
 };
+
+export const filenameHasExtension = (filename, extension) => {
+  return filename.split('.').length === 2 && filename.includes(extension);
+}
+
