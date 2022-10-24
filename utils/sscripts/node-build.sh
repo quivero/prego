58a3f283f74f96b2a39bc807194edac6fdd3c400
Bruno Peixoto <brunolnetto@gmail.com>
2022-10-24 15:19:28 -0300

    config/ build command

---
+++ utils/sscripts/node-build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo yum install curl -y 
+
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+source ~/.bashrc
+
+curl –sL https://rpm.nodesource.com/setup_10.x | sudo bash -
+
+sudo yum install –y nodejs
+
+nvm install lts/*
+
+echo `NPM version: $(npm -v)`
+echo `NVM version: $(nvm -v)`
+echo `NodeJS version: $(node -v)`
+
