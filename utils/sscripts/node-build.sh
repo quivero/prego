2284371349a479c3b0c45c0cec2b005992cdb1a5
EC2 Default User <ec2-user@ip-172-31-6-11.us-west-2.compute.internal>
2022-10-24 18:53:21 +0000

    config/ build node libraries

--- utils/sscripts/node-build.sh
+++ utils/sscripts/node-build.sh
@@ -6,12 +6,11 @@ curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 source ~/.bashrc
 
 curl –sL https://rpm.nodesource.com/setup_10.x | sudo bash -
-
 sudo yum install –y nodejs
 
 nvm install lts/*
 
-echo `NPM version: $(npm -v)`
-echo `NVM version: $(nvm -v)`
-echo `NodeJS version: $(node -v)`
+echo "NPM version: $(npm -v)"
+echo "NVM version: $(nvm --version)"
+echo "NodeJS version: $(node -v)"
 
