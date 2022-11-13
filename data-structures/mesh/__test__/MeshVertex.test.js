24f754f2a7b567829301288c6836f94cb7f8fbc5
Bruno Peixoto <brunolnetto@gmail.com>
2022-11-12 22:05:20 -0300

    test/ throw error and create vertices

--- data-structures/mesh/__test__/MeshVertex.test.js
+++ data-structures/mesh/__test__/MeshVertex.test.js
@@ -1,4 +1,5 @@
 import MeshVertex from "../MeshVertex.js";
+import { createMVertices } from "../MeshVertex.js";
 
 describe("GraphicMeshVertex", () => {
   it("should throw an error when trying to create MeshVertex", () => {
@@ -14,9 +15,18 @@ describe("GraphicMeshVertex", () => {
 
   it("should create graph vertex", () => {
     const gvertex = new MeshVertex("A", [1, 2]);
-
-    expect(gvertex).toBeDefined();
-    expect(gvertex.toString()).toBe("A");
     expect(gvertex.getKey()).toBe("A");
+  });
+
+  it("should create graph vertex", () => {
+    createMVertices(["A"], [[1, 2]]);
+  });
+
+  it("should throw error for unequal array lengths", () => {
+    function throwErrorUnequalArrayLengths() {
+      createMVertices(["A", "b"], [[1, 2]]);
+    }
+
+    expect(throwErrorUnequalArrayLengths).toThrowError();
   });
 });
