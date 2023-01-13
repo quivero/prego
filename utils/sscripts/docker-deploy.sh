e5d233c488643902fcc45308d7b6e6bcadfd795c
Bruno Peixoto <brunolnetto@gmail.com>
2023-01-13 18:39:47 -0300

    lint/ add double quotes on script variables

--- utils/sscripts/docker-deploy.sh
+++ utils/sscripts/docker-deploy.sh
@@ -8,7 +8,7 @@ if [[ $# -eq 0 ]]; then
   echo "Provide Docker image name, local listen port and output port!"
 
 elif docker ps -a | grep -qw "$1"; then
-  container_id=$(docker ps -a | grep -w $1 | head -n1 | awk "{print $1;}");
+  container_id=$(docker ps -a | grep -w "$1" | head -n1 | awk "{print $1;}");
 
   echo "Container image $IMAGE_NAME with ID $container_id already exists! Watch it below."
   bash "$(pwd)/scripts/docker-watch.sh" "$container_id"
