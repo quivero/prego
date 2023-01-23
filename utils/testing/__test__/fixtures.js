e993d5dbb0b85f837fd9e67ba11cf2097f25442a
Bruno Peixoto <brunolnetto@gmail.com>
2023-01-22 22:06:48 -0300

    feat/ assertions and fixtures

---
+++ utils/testing/__test__/fixtures.js
@@ -0,0 +1,49 @@
+import _ from 'lodash'
+import { 
+    buildAssertion, 
+    buildScenario,
+    batchAssert 
+} from '../assertions'
+
+/* 
+ * assert fixtures
+ */
+export const assertFixtures = [
+    [ 42,         (result) => expect(result).toBeDefined()            ],
+    [ "42", "42", (result, expected) => expect(result).toBe(expected) ],
+];
+
+// Valid samples
+export const validAssertLength1Item = assertFixtures[0];
+export const validAssertLength2Item = assertFixtures[1];
+
+// Error-prone samples
+export const invalidAssertItemLength = [ '42' ];
+export const invalidAssertCallbackItem = [ '42', '42' ];
+
+/* 
+ * atest fixtures
+ */
+export let validAtestScenario;
+let setup, prepare, exercise, verify, teardown;
+
+export let validAtestFixture = '42';
+const resultFunction = (fixture_) => fixture_;
+let expectedAtestResult = '42';
+
+setup = () => {};
+
+prepare = (fixture_) => fixture_;
+
+exercise = (resources) => {
+    return buildAssertion(
+        resultFunction(resources),
+        expectedAtestResult,
+        (result, expected) => expect(result).toBe(expected)
+    );
+};
+
+teardown = () => {};
+
+validAtestScenario = buildScenario(setup, prepare, exercise, teardown);
+
--- utils/testing/assertions.js
+++ utils/testing/assertions.js
@@ -1,46 +1,100 @@
+import _ from 'lodash'
+
 export const assert = (item) => {
-  let result, expected;
-  const item_length = item.length;
+    let result, expected;
+    const item_length = item.length;
+    const isValidAssertItem = item_length === 2 || item_length === 3;
 
-  if (item_length === 2 || item_length === 3) {
-    const callback = item[item_length - 1];
+    if(isValidAssertItem) {
+        const callback = item[item_length-1];
+        
+        if(typeof callback === 'function') {
+            switch(item_length){
+                case 2:
+                    result = item[0];
+        
+                    callback(result);
+        
+                    break;
+                    
+                case 3:
+                    result = item[0];
+                    expected = item[1];
+        
+                    callback(result, expected);
+        
+                    break;
+            }
 
-    if (typeof callback === "function") {
-      switch (item_length) {
-        case 2:
-          result = item[0];
+        } else {
+            const callbackValidity = 'Last element on item must be a callback function!';
+            
+            throw Error(callbackValidity);
+        }
 
-          callback(result);
-
-          break;
-
-        case 3:
-          result = item[0];
-          expected = item[1];
-
-          callback(result, expected);
-
-          break;
-      }
     } else {
-      const callbackValidity =
-        "Last element on item must be a callback function!";
+        const description = 'Test element may have structure: ';
+        const validArgument_1 = '[result, assertion_callback]';
+        const validArgument_2 = '[result, expected, assertion_callback]';
 
-      throw Error(item);
+        const schemas = `${validArgument_1} or ${validArgument_2}`;
+
+        throw Error(description + schemas);
+    }   
+}
+
+export const batchAssert = (items) => items.forEach(item => assert(item));
+
+const verifyExperiment = (experiments) => {
+    batchAssert(_.zip(
+        experiments.results,
+        experiments.expectations,
+        experiments.assertionMaps
+    ));
+};
+
+export const atest = (fixtures, scenario) => {
+    scenario.setup();
+
+    const experiment = scenario.exercise(
+        scenario.prepare(fixtures)
+    );
+    
+    verifyExperiment(experiment);
+    
+    scenario.teardown();
+}
+
+export const batchAtest = (fixtures, scenarios) => {
+    let scenario, fixture;
+    
+    _.zip(fixtures, scenarios).forEach( 
+        ( scenario_tuple ) => {
+            fixture = scenario_tuple[0];
+            scenario = scenario_tuple[1];
+            
+            atest(fixture, scenario)
+        } 
+    );
+};
+
+export const buildScenario = (setup, prepare, exercise, teardown) => {
+    return {
+        "setup": setup, 
+        "prepare": prepare,
+        "exercise": exercise,
+        "teardown": teardown
+
     }
-  } else {
-    const description = "Test element may have structure: ";
-    const schemas =
-      "[result, assertion_callback] or [result, expected, assertion_callback]";
+}
 
-    throw Error(description + schemas);
-  }
-};
-
-export const batchAssert = (items) => {
-  expect.assertions(items.length);
-  items.forEach((item) => assert(item));
-};
+export const buildAssertion = (results, expectations, assertionMaps) => {
+    return {
+        "results": results, 
+        "expectations": expectations,
+        "assertionMaps": assertionMaps
+    }
+}
 
 export const toBeAssert = (result, expected) => expect(result).toBe(expected);
 export const toBeEqualAssert = (result, expected) =>
